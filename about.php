9e512fb719efd9b158868be0203842fb9c2c4688
Kazion500 <64147010+Kazion500@users.noreply.github.com>
2020-05-18 11:20:48 +0200

    Create about.php

---
+++ about.php
@@ -0,0 +1,27 @@
+<?php 
+include 'header.php';
+include 'nav.php';
+?>
+
+<section>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-8">
+				
+			</div>
+		</div>
+	</div>
+</section>
+
+
+
+
+
+
+
+<?php 
+require 'script.php';
+?>
+
+</body>
+</html>
